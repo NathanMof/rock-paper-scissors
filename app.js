9fb671da5eb5fcefa0084a13e194209654f7c4f7
NathanMof <nathanmofarah@gmail.com>
2023-06-10 19:46:15 -0700

    added game function to loop game and to validata prompt

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 
-const getRandomInt = (max) => {
-    return Math.floor(Math.random() * max);
+let score = 0
+
+
+const getRandomInt = () => {
+    return Math.floor(Math.random() * 3);
 }
-let result = getRandomInt(3);
 
 
-const getComputerChoice = () => {
+
+const getComputerChoice = (result) => {
     switch (result) {
         case 0:
             return "rock"
@@ -17,33 +20,67 @@ const getComputerChoice = () => {
 }
 
 
-let playerSelection = prompt("Enter Rock Paper or Scissors").toLowerCase();
-const computerSelection = getComputerChoice(result);
-
 const playRound = (playerSelection, computerSelection) => {
     switch (playerSelection) {
         case computerSelection:
             return `It's a tie! You both picked ${playerSelection}`
         case "rock":
             if (computerSelection === "scissors") {
+                score++;
                 return "You win! rock beats scissors"
             }
             break;
         case "paper":
             if (computerSelection === "rock") {
+                score++;
                 return "You win! paper beats rock";
             }
             break;
         case "scissors":
             if (computerSelection === "paper") {
-                return "You win! scissors beaters paper";
+                score++;
+                return "You win! scissors beats paper";
             }
             break;
     }
     return `You lose! ${computerSelection} beats ${playerSelection}`
 }
 
-console.log(playRound(playerSelection, computerSelection));
+
+
+const game = () => {
+    for (let i = 0; i <= 4; i++) {
+        let check = false
+        let result = getRandomInt();
+        const computerSelection = getComputerChoice(result);
+        let playerSelection = null;
+        while (!check) {
+            playerSelection = prompt("Enter Rock Paper or Scissors").toLowerCase();
+            if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+                check = true
+            } else alert("Wrong input please type \"rock\", \"paper\" or \"scissors\"")
+        }
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    console.log(`Your final score is ${score}`);
+}
+
+game();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
